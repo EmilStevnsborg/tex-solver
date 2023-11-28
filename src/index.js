f7bf89f64a235515be1b26450ff3533fb724910f
EmilStevnsborg <emilstevnsborg.official@gmail.com>
2023-11-28 19:34:43 +0100

    changed browserrouter

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename="/tex-solver/">
     <App/>
   </BrowserRouter>,
   document.getElementById('root')
